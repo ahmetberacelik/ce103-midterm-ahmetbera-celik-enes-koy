cb3426f5a701a519dafa091c5e7687b33de57eb2
Eneskoy <koy.eness@gmail.com>
2023-11-12 16:20:46 +0300

    In hospitalsystemapp.cpp, I started writing codes and I added a login system.

--- src/hospitalsystemapp/src/hospitalsystemapp.cpp
+++ src/hospitalsystemapp/src/hospitalsystemapp.cpp
@@ -10,6 +10,34 @@ using namespace std;
 
 int main()
 {
-	
+	cout << "WELCOME TO HOSPITAL APPOINTMENT AND MANAGEMENT SYSTEM\n";
+	write_apostrophe();
+	while (true)
+	{
+		cout << "Please select your user type number.\n" << "1--> Admin\n2--> Doctor\n3--> Patient\n";
+		write_apostrophe();
+		int user_type;
+		cin >> user_type;
+		if (user_type == 1)
+		{
+			break;
+		}
+		else if (user_type == 2)
+		{
+			break;
+		}
+		else if (user_type == 3)
+		{
+			cin.ignore();
+			patient_registration();
+			patient_menu();
+			break;
+		}
+		else
+		{
+			cout << "You've entered wrong option number. Please try again...\n";
+			continue;
+		}
+	}
 	return 0;
 }
