804f5e47fd7571f77ec1729d140c2f29b2bbee80
Eneskoy <koy.eness@gmail.com>
2023-11-16 17:06:24 +0300

    comment lines added and edited

--- src/hospitalsystem/header/hospitalsystem.h
+++ src/hospitalsystem/header/hospitalsystem.h
@@ -4,22 +4,21 @@
 #include "../../utility/header/commonTypes.h"
 using namespace std;
 
-struct Informations
-{
-	string patient_name;
-	string patient_medical_history;
-	string default_patient_names[2] = { "Radi Yilmaz","Ayse Batmaz" };
-	string doc[4] = { "Ahmet Bera Celik","Enes Koy","Hasan Basri Taskin","Yakup Eroglu" };
-	string nurse[4] = { "Gamze Kaplan","Merve Demir","Sevda Özturk","Bilgi Vural" };
-	string patient_treatment_session[8];
-	string drug_features[3];
+struct Informations {
+  string patient_name;
+  string patient_medical_history;
+  string default_patient_names[2] = { "Radi Yilmaz","Ayse Batmaz" };
+  string doc[4] = { "Ahmet Bera Celik","Enes Koy","Hasan Basri Taskin","Yakup Eroglu" };
+  string nurse[4] = { "Gamze Kaplan","Merve Demir","Sevda Özturk","Bilgi Vural" };
+  string patient_treatment_session[8];
+  string drug_features[3];
 };
 
 int write_apostrophe();
 
 int login();
 
-int patient_registration(string login_patient_name, string login_patient_medical_history);
+int patient_registration();
 int patient_menu();
 int apointment_scheduling();
 int patient_management();
@@ -29,7 +28,5 @@ int integrations();
 int doctor_menu();
 int prescription_management();
 int patient_progress_tracking();
-
-int switch_user();
 
 #endif // HOSPITALSYSTEM_H
